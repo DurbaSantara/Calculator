5456c1cf0c21721058fa87b32a1bc935e4112564
santa <santa@DESKTOP-17LV42G>
2019-05-11 10:47:58 +0530

    Completed Successfully

--- src/main/java/main/Calculator.java
+++ src/main/java/main/Calculator.java
@@ -1,18 +1,19 @@
 package main;
-import java.util.*; 
+import java.util.*;
 
 public class Calculator {
     public static void main(String[] args)
     {
         System.out.println("Enter first and second number:");
-        Scanner inp= new Scanner(System.in);
+        @SuppressWarnings("resource")
+		Scanner sc= new Scanner(System.in);
         int num1,num2;
-        num1 = inp.nextInt();
-        num2 = inp.nextInt();
-        int ans;
+        num1 = sc.nextInt();
+        num2 = sc.nextInt();
+        
         System.out.println("Enter your selection: 1 for Addition, 2 for substraction 3 for Multiplication and 4 for division:");
         int choose;
-        choose = inp.nextInt();
+        choose = sc.nextInt();
         switch (choose){
         case 1:
             System.out.println(add( num1,num2));
@@ -38,26 +39,26 @@ public class Calculator {
     public static int add(int x, int y)
     {
     	int result=0;
-//        write your code here 
+    	result=x+y;
     	
         return result;
     }
     public static int sub(int x, int y)
     {
     	int result=0;
-//        write your code here
+    	result=x-y;
         return result;
     }
     public static int mult(int x, int y)
     {
     	int result=0;
-//      write your code here        
+    	result=x*y;
     	return result;
     }
     public static int div(int x, int y)
     {
     	int result=0;
-//      write your code here
+    	result=x/y;
     	return result;
     }
 
